a7d98b08bef38fc55d21a671b74f285d9687ed77
Luca Torella <luca@blinkbox.com>
2014-07-22 08:01:03 +0100

    Initial Commit

---
+++ FormValidator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FormValidator
+//
+//  Created by Luca Torella on 22/07/2014.
+//  Copyright (c) 2014 Luca Torella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
